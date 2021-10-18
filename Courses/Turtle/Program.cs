e51eda263bd51c35c40c62a01225901fae3afa18
Alexander Gribanov <86486142+XanderJB@users.noreply.github.com>
2021-10-18 11:57:49 +0300

    Add files via upload

---
+++ Courses/Turtle/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.SmallBasic.Library;
+namespace TurtleGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Turtle.Move(10);
+            }
+        }
+    }
+}
