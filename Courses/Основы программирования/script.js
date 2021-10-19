e6b3f0c80f9cc8e8327de1b8ef6bcc88316b6db5
Alexander Gribanov <86486142+XanderJB@users.noreply.github.com>
2021-10-19 10:06:20 +0300

    Update script.js

--- Courses/Основы программирования/script.js
+++ Courses/Основы программирования/script.js
@@ -1,3 +1,8 @@
+alert ("Hello, World!");
+
+var name = prompt("Enter your name: ");
+alert ("Hello" + name);
+
 var a = prompt("Enter 1st number: ");
 var b = prompt("Enter 2nd number: ");
 var c = a * b;
