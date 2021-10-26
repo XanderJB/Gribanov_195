a9e1fa6183f24db4108c1591236137ca51f821fb
Alexander Gribanov <86486142+XanderJB@users.noreply.github.com>
2021-10-26 08:57:09 +0300

    Update script.js

--- Courses/Основы программирования/script.js
+++ Courses/Основы программирования/script.js
@@ -18,8 +18,6 @@ alert ("Частное чисел равно: " + c);
 c = (a + b) / 2;
 alert ("Среднее арифметическое чисел равно: " + c);*/
 
-
-
 /*var baseLength_1 = prompt("Введите длину основания трапеции а: "); //пользователь вводит длину первого основания трапеции
 var baseLength_2 = prompt("Введите длину основания трапеции b: "); //пользователь вводит длину второго основания трапеции
 var trapezoidHeight = prompt("Введите высоту трапеции h: "); //пользователь вводит высоту трапеции
@@ -30,8 +28,6 @@ trapezoidHeight = parseInt(trapezoidHeight);
 
 var trapezoidArea = trapezoidHeight*(baseLength_1+baseLength_2)/2; //мы задаем компьютеру формулу, по которой он должен посчитать площадь трапеции
 alert("Площадь трапеции равна " + trapezoidArea); //вывод значения площади трапеции*/
-
-
 
 /*var rouble = prompt("Введите сумму в рублях: "); //сумма в рублях которую вводит пользователь
 var dollar = 70.96; //сегодняшний курс доллара
@@ -48,7 +44,7 @@ var Itog_1 = rouble / euro; //вычисление количества рубл
 alert("Итого: " + Itog_1.toFixed(2) + " евро"); */
 
 
-var a = +prompt("Enter 1st number: ")
+/*var a = +prompt("Enter 1st number: ")
 var b = +prompt("Enter 2nd number: ")
 
 var max = a
@@ -60,4 +56,47 @@ if (a > b) {
  else{
     max = b
 }
-alert(max)
+alert(max)*/
+
+/*for (var i = 0; i < 1000; i++){
+    document.write("ABOBA </br>")
+}*/
+
+var userAnswer = prompt("C чем вкуснее есть котлетки?")
+var score = 0
+userAnswer.toLowerCase();
+if (userAnswer == "с пюрешкой" || userAnswer == "просто так" || userAnswer == "с хлебом"){
+    alert ("Вы угадали, ура)")
+    score++
+}else{
+    alert("Вы не угадали")
+}
+userAnswer = prompt("что видят с закрытыми глазами?")
+userAnswer.toLowerCase();
+if (userAnswer == "сны" || userAnswer == "сон" || userAnswer == "сновидения"){
+    alert ("Вы угадали, ура)")
+    score++
+}else{
+    alert("Вы не угадали")
+    
+}
+userAnswer = prompt("что бьют лентяи?")
+userAnswer.toLowerCase();
+if (userAnswer == "баклуши" || userAnswer == "груши"){
+    alert ("Вы угадали, ура)")
+    score++
+}else{
+    alert("Вы не угадали")
+    
+}
+var HighScore = score
+if (score == 3){
+    alert("Ваше количество баллов: " + score + ". Поздравляю, вы выиграли")
+}else if (score == 1 || score == 2){
+    alert("Ваше количество баллов: " + score + 
+   ". Вы играли хорошо, но можете еще лучше. Обновите страницу и попробуйте еще раз.")
+}
+else{
+    alert("К сожалению, вы не набрали ни одного балла и проиграли. Обновите страницу и попробуйте еще раз.")
+}
+
